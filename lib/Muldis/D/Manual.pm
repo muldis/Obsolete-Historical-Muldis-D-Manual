4772202ee45effcb55c0fe918171d109d381ad1b
Darren Duncan <darren@DarrenDuncan.net>
2010-05-11 18:50:30 -0700

    this is what will end up on CPAN as Muldis::D::Manual version 0.3.0

--- lib/Muldis/D/Manual.pm
+++ lib/Muldis/D/Manual.pm
@@ -2,7 +2,7 @@ use 5.006;
 use strict;
 use warnings;
 package Muldis::D::Manual;
-our $VERSION = '0.002000';
+our $VERSION = '0.003000';
 $VERSION = eval $VERSION;
 # Note that Perl code only exists at all in this file in order to help
 # the CPAN indexer handle the distribution properly.
@@ -20,7 +20,7 @@ Muldis D language and implementations manual
 
 =head1 VERSION
 
-This document is Muldis::D::Manual version 0.2.0.
+This document is Muldis::D::Manual version 0.3.0.
 
 =head1 DESCRIPTION
 
